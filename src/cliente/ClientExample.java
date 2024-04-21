39cda1b6679899d75da670df2a9f1a438a756100
Humberto <humbertovitalino1@gmail.com>
2024-04-21 10:06:14 -0300

    adição do novo servidor na chamada do client

--- src/cliente/ClientExample.java
+++ src/cliente/ClientExample.java
@@ -10,6 +10,12 @@ public class ClientExample {
             stub.connectToService();
             System.out.println(stub.sendMessage("Hello, Server!"));
             stub.close();
+            
+            
+        	stub = new ServiceStub("ServiceChatbot");
+            stub.connectToService();
+            System.out.println(stub.sendMessage("Hi"));
+            stub.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
